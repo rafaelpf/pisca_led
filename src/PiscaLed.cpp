d54ca6dc53ddd7734f12d9089addbeb68f72b6c9
Rafael de Paiva Ferreira <rafaeldpferreira@yahoo.com.br>
2020-07-07 17:57:39 -0300

    Alteracao 2

--- src/PiscaLed.cpp
+++ src/PiscaLed.cpp
@@ -1,13 +1,19 @@
 #include <PiscaLed.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int _pino;
 long _time;
 long _time2;
+int _pino2;
+bool x;
+int cont;
 PiscaLed::PiscaLed(int pino, long time, long time2){
     pinMode(pino, OUTPUT);
     _pino = pino;
     _time = time;
     _time2 = time2;
+    
 
 }
 
@@ -20,10 +26,11 @@ void PiscaLed::acende_led(){
 
 }
 
-void PiscaLed::acende_led2(){
-
-    digitalWrite(_pino, LOW);
-    delay(_time2);
+void PiscaLed::acende_led2()
+{
     digitalWrite(_pino, HIGH);
     delay(_time2);
-} 
+    digitalWrite(_pino, LOW);
+    delay(_time2);
+    
+   } 
